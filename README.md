cf84dd1b141b653de359ba7c37ca2fca49bedc78
sujay.png <147098677+sujay-png@users.noreply.github.com>
2026-02-22 21:45:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# billing_software
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/billing_software/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.billing_software
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
